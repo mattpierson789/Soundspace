8e35495aacec9d392bd8571c8ff237bd699af58a
vinicozzi <vinicozzimincent@gmail.com>
2023-06-13 08:05:11 -0400

    css changes-splash

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -1,14 +1,15 @@
-
 import React from 'react';
+import './MainPage.css'
 
 function MainPage() {
     return (
       <>
-        
+        <div className="splash-container">
         <img id='splash-image' className="center" src='https://soundspace-seeds.s3.amazonaws.com/public/Soundspace-Splash-Image' />
         <footer>
           Copyright &copy;  Soundspace 2023
         </footer>
+        </div>
       </>
     );
   }
