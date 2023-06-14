6201e1b88adb12584dba2b7cce945974e4039b6b
vinicozzi <vinicozzimincent@gmail.com>
2023-06-14 17:42:55 -0400

    css header changes

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+// import MainFeed from '../MainFeed/MainFeed';
 import './Header.css';
 
 function Header() {
+
+    const currentLocation = useSelector((state) => state.session.currentUser.location)
+    const [location, setLocation] = useState(currentLocation)
+
+    const handleClick = (value) => {
+        if (location !== value) setLocation(value);
+    }
+
   return (
     <div className="header-container">
       <div className="search-bar">
@@ -10,6 +20,13 @@ function Header() {
       <div className="header-index-banner">
         <h1>Your Feed</h1>
       </div>
+      <div className="mainfeed-city-filters">
+            <button value={"NYC"} onClick={(e) => handleClick(e.target.value)}>NYC</button>
+            <button value={"LA"} onClick={(e) => handleClick(e.target.value)}>LA</button>
+            <button value={"ATL"} onClick={(e) => handleClick(e.target.value)}>ATL</button>
+            <button value={"Global"} onClick={(e) => handleClick(e.target.value)}>Global</button>
+        </div>
+        {/* <MainFeed location={location} /> */}
     </div>
   );
 }
--- frontend/src/components/MainFeed/MainFeed.js
+++ frontend/src/components/MainFeed/MainFeed.js
@@ -2,26 +2,14 @@ import React, { useState } from 'react';
 import Tracks from '../Tracks/Tracks.js';
 import NavBar from '../NavBar/SidebarLinks.js';
 import Header from '../Header/Header.js';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchTracks } from '../../store/tracks.js';
+import { useParams } from 'react-router-dom';
+
+// import { fetchTracks } from '../../store/tracks.js';
 // import MusicBar from '../MusicBar/MusicBar.js';
 
 function MainFeed() {
-const dispatch = useDispatch();
-const currentLocation = useSelector((state) => state.session.currentUser.location)
-const [location, setLocation] = useState(currentLocation)
-debugger
-    useEffect(() => {
-        debugger
-        dispatch(fetchTracks());
-        debugger
-      }, [dispatch]);
-    
-    const handleClick = (value) => {
-        debugger
-        if (location !== value) setLocation(value);
-    }
+
+    const { location } = useParams;
 
     return (
         <div className="mainfeed-container">
@@ -29,13 +17,8 @@ debugger
                 <NavBar />
             </div>
             <div className="mainfeed-header">
-                <Header />
+                <Header/>
             </div>
-            <button value={"NYC"} onClick={(e) => handleClick(e.target.value)}>NYC</button>
-            <button value={"LA"} onClick={(e) => handleClick(e.target.value)}>LA</button>
-            <button value={"ATL"} onClick={(e) => handleClick(e.target.value)}>ATL</button>
-            <button value={"Global"} onClick={(e) => handleClick(e.target.value)}>Global</button>
-         
             <div className="mainfeed-index">
                 <Tracks location={ location }/>
             </div>
