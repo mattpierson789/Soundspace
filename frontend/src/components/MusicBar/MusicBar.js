b9cc9a5b3b399f5799400146a4b6c78314e20a07
jliangrpb <jeremy.liang612@gmail.com>
2023-06-15 09:55:21 -0400

    musicbar prev and next buttons

--- frontend/src/components/MusicBar/MusicBar.js
+++ frontend/src/components/MusicBar/MusicBar.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import AudioPlayer, { RHAP_UI } from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import './MusicBar.css';
+import { setCurrentTrack } from '../../store/audio';
 
 function MusicBar() {
+  const dispatch = useDispatch();
   const currentTrack = useSelector((state) => state.audio.currentTrack);
   const user = useSelector((state) => state.session.currentUser);
+  const tracks = useSelector((state) => state.tracks.allTracks);
 
   if (!currentTrack || !user) {
     return null;
@@ -14,9 +17,24 @@ function MusicBar() {
 
   const { title, artist, trackUrl, trackImageUrl } = currentTrack;
 
+  const handleClickNext = () => {
+    const currentIndex = tracks.findIndex((track) => track === currentTrack);
+    const nextIndex = (currentIndex + 1) % tracks.length;
+    dispatch(setCurrentTrack(tracks[nextIndex]));
+  };
+
+  const handleClickPrevious = () => {
+    const currentIndex = tracks.findIndex((track) => track === currentTrack);
+    const previousIndex = (currentIndex - 1 + tracks.length) % tracks.length;
+    dispatch(setCurrentTrack(tracks[previousIndex]));
+  };
+
   return (
     <div className='footer-container'>
       <div className="play-bar-track-info">
+        <div className="now-playing-image-container">
+          {trackImageUrl && <img src={trackImageUrl} alt="" />}
+        </div>
         <div className="now-playing-section">
           <div className="now-playing-artist-info">
             <p className="now-playing-artist-info-name">{title}</p>
@@ -25,14 +43,15 @@ function MusicBar() {
         </div>
       </div>
       <div className="play-bar-control-container">
-        <AudioPlayer 
-          src={trackUrl} 
+        <AudioPlayer
+          showSkipControls
+          src={trackUrl}
           layout="stacked-reverse"
           autoPlay={true}
           showFilledVolume={true}
           style={{
             width: '100%',
-            height: '100%'
+            height: '100%',
           }}
           customControlsSection={[RHAP_UI.MAIN_CONTROLS]}
           customProgressBarSection={[
@@ -41,6 +60,9 @@ function MusicBar() {
             RHAP_UI.DURATION,
             RHAP_UI.VOLUME,
           ]}
+          onClickNext={handleClickNext}
+          onClickPrevious={handleClickPrevious}
+          onEnded={handleClickNext}
         />
       </div>
     </div>
