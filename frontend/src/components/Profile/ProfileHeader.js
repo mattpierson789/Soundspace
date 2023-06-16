2504a6156b1ee3b350aca660265e1a7f00582b4b
Matt Pierson <matt.pierson789@gmail.com>
2023-06-16 00:32:01 -0400

    bug fix profile headeer

--- frontend/src/components/Profile/ProfileHeader.js
+++ frontend/src/components/Profile/ProfileHeader.js
@@ -35,14 +35,14 @@ const ProfileHeader = ({ onFilterValue, filterValue }) => {
     dispatch(fetchUserFollows(username)).then(() => {
       dispatch(fetchUserFollowing(username)).then(() => {
         const isCurrentUserFollower = userFollowers.find(follower => follower._id === currentUser._id) !== undefined;
-        if(isMountedRef.current) {
+        if (isMountedRef.current) {
           setIsFollowing(isCurrentUserFollower);
         }
       });
     });
     return () => {
       isMountedRef.current = false;
-    }
+    };
   }, [dispatch, username, userFollowers, currentUser]);
 
   const openModal = () => {
@@ -62,7 +62,7 @@ const ProfileHeader = ({ onFilterValue, filterValue }) => {
       history.push(`/profile/${user.username}`);
       setShowFollowModal(false);
     }
-  }
+  };
 
   useEffect(() => {
     const handleOutsideClick = (e) => {
@@ -81,128 +81,117 @@ const ProfileHeader = ({ onFilterValue, filterValue }) => {
   return (
     <>
       <div className="profile-header">
-      <div className="track-filters">
-            <button
-              value="All"
-              onClick={(e) => onFilterValue(e.target.value)}
-              className={filterValue === 'All' ? 'active' : ''}
-            >
-              All
-            </button>
-            <button
-              value="Original"
-              onClick={(e) => onFilterValue(e.target.value)}
-              className={filterValue === 'Original' ? 'active' : ''}
-            >
-              Original
-            </button>
-            <button
-              value="Reposts"
-              onClick={(e) => onFilterValue(e.target.value)}
-              className={filterValue === 'Reposts' ? 'active' : ''}
-            >
-              Reposts
-            </button>
+        <div className="track-filters">
+          <button
+            value="All"
+            onClick={(e) => onFilterValue(e.target.value)}
+            className={filterValue === 'All' ? 'active' : ''}
+          >
+            All
+          </button>
+          <button
+            value="Original"
+            onClick={(e) => onFilterValue(e.target.value)}
+            className={filterValue === 'Original' ? 'active' : ''}
+          >
+            Original
+          </button>
+          <button
+            value="Reposts"
+            onClick={(e) => onFilterValue(e.target.value)}
+            className={filterValue === 'Reposts' ? 'active' : ''}
+          >
+            Reposts
+          </button>
         </div>
         <div className="profile-info">
-        <div className="image-container-showPage">
-          {showUser && <img className="showPage-profilePageImg" src={showUser.profileImageUrl} />}
-\          </div>
+          <div className="image-container-showPage">
+            {showUser && <img className="showPage-profilePageImg" src={showUser.profileImageUrl} alt={showUser.username} />}
+          </div>
           <div className="follower-info">
             {currentUser.username !== username && (
               <button onClick={handleFollow}>
                 {isFollowing ? 'Unfollow' : 'Follow'}
               </button>
             )}
-            {username === currentUser.username && <button onClick={openModal}>Create A Post</button>}
-              <div className="showUser-username">
-              {showUser.username}
-              </div>
-              <div className="showUser-location">
-              {showUser.location}
-              </div>
-            <button onClick={openModal}>Create A Post</button>
+            <div className="showUser-username">
+              {showUser && showUser.username}
+            </div>
+            <div className="showUser-location">
+              {showUser && showUser.location}
+            </div>
+            {username === currentUser.username && (
+              <button onClick={openModal}>Create A Post</button>
+            )}
             <div onClick={openFollowModal}>{followers.length} Followers</div>
             <div onClick={openFollowModal}>{following.length} Following</div>
           </div>
-          {showModal && (
-            <div className="modal">
-              <div className="modal-content">
-                <span className="close" onClick={closeModal}>
-                  &times;
-                </span>
-                <CreatePostModal closeModal={closeModal} />
-              </div>
-            </div>
-          )}
-  
-          {showFollowModal && (
-            <div className="follow-modal">
-              <div className="follow-modal-content">
-                <div className="modal-header">
-                  <button
-                    className={`toggle-button ${followType === 'followers' ? 'active' : ''}`}
-                    onClick={() => setFollowType('followers')}
-                  >
-                    Followers
-                  </button>
-                  <button
-                    className={`toggle-button ${followType === 'following' ? 'active' : ''}`}
-                    onClick={() => setFollowType('following')}
-                  >
-                    Following
-                  </button>
-                </div>
-                <div className="modal-body">
-                  {followType === 'followers' && (
-                    <div className="followers-content">
-                      {followers.map((follower) => {
-                        if (follower) {
-                          return (
-                            <div className="followers-content-box" key={follower._id} onClick={() => handleShowPage(follower)}>
-                              <img className="followers-content-img" src={follower.profileImageUrl} alt={follower.username} />
-                              <span className="followers-content-box-handle">{follower.username}</span>
-                            </div>
-                          );
-                        }
-                        return null;
-                      })}
-                    </div>
-                  )}
-                  {followType === 'following' && (
-                    <div className="following-content">
-                      {following.map((celeb) => {
-                        if (celeb) {
-                          return (
-                            <div className="followers-content-box" key={celeb._id} onClick={() => handleShowPage(celeb)}>
-                              <img className="followers-content-img" src={celeb.profileImageUrl} alt={celeb.username} />
-                              <span className="followers-content-box-handle">{celeb.username}</span>
-                            </div>
-                          );
-                        }
-                        return null;
-                      })}
-                    </div>
-                  )}
-                </div>
-              </div>
-            </div>
-          )}
+        </div>
+      </div>
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <CreatePostModal closeModal={closeModal} />
           </div>
         </div>
-        {showModal && (
-          <div className="modal">
-            <div className="modal-content">
-              <span className="close" onClick={closeModal}>
-                &times;
-              </span>
-              <CreatePostModal closeModal={closeModal} />
+      )}
+      {showFollowModal && (
+        <div className="follow-modal">
+          <div className="follow-modal-content">
+            <div className="modal-header">
+              <button
+                className={`toggle-button ${followType === 'followers' ? 'active' : ''}`}
+                onClick={() => setFollowType('followers')}
+              >
+                Followers
+              </button>
+              <button
+                className={`toggle-button ${followType === 'following' ? 'active' : ''}`}
+                onClick={() => setFollowType('following')}
+              >
+                Following
+              </button>
+            </div>
+            <div className="modal-body">
+              {followType === 'followers' && (
+                <div className="followers-content">
+                  {followers.map((follower) => {
+                    if (follower) {
+                      return (
+                        <div className="followers-content-box" key={follower._id} onClick={() => handleShowPage(follower)}>
+                          <img className="followers-content-img" src={follower.profileImageUrl} alt={follower.username} />
+                          <span className="followers-content-box-handle">{follower.username}</span>
+                        </div>
+                      );
+                    }
+                    return null;
+                  })}
+                </div>
+              )}
+              {followType === 'following' && (
+                <div className="following-content">
+                  {following.map((celeb) => {
+                    if (celeb) {
+                      return (
+                        <div className="followers-content-box" key={celeb._id} onClick={() => handleShowPage(celeb)}>
+                          <img className="followers-content-img" src={celeb.profileImageUrl} alt={celeb.username} />
+                          <span className="followers-content-box-handle">{celeb.username}</span>
+                        </div>
+                      );
+                    }
+                    return null;
+                  })}
+                </div>
+              )}
             </div>
           </div>
-        )}
+        </div>
+      )}
     </>
   );
-  
 };
 
 export default ProfileHeader;
