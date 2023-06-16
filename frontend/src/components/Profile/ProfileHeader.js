39acfbc52f89989fc1c5e2766e9c345779ff9357
vinicozzi <vinicozzimincent@gmail.com>
2023-06-15 20:33:04 -0400

    styles

--- frontend/src/components/Profile/ProfileHeader.js
+++ frontend/src/components/Profile/ProfileHeader.js
@@ -9,11 +9,7 @@ import CreatePostModal from '../UploadTextPost/UploadTextPost';
 
 const ProfileHeader = ({ onFilterValue, filterValue }) => {
   const dispatch = useDispatch();
-<<<<<<< HEAD
   const { username } = useParams();
-=======
-  const { userId, username } = useParams();
->>>>>>> vince-style
   const currentUser = useSelector(state => state.session.currentUser);
   const userFollowers = useSelector(state => state.follow.followers);
   const isCurrentUserFollower = userFollowers.find(follower => follower._id === currentUser._id) !== undefined;
@@ -21,15 +17,9 @@ const ProfileHeader = ({ onFilterValue, filterValue }) => {
   const userTracks = useSelector(state => state.tracks.userTracks);
   const showUser = useSelector(state => state.session.allUsers.find(user => user.username === username ));
 
-  // const originalTracks = userTracks.filter(track => track.artist === userId);
-  // const reposts = userTracks.filter(track => track.artist !== userId);
-
   const [showModal, setShowModal] = useState(false);
 
-  // const [filter, setFilter] = useState('All');
-
   const handleFilter = (value) => {
-    // setFilter(value);
     onFilterValue(value);
   };
 
@@ -51,7 +41,6 @@ const ProfileHeader = ({ onFilterValue, filterValue }) => {
     dispatch(fetchUserFollows(username));
     dispatch(fetchUserFollowing(username));
   }, [dispatch, username]);
-  debugger
 
   const openModal = () => {
     setShowModal(true);
@@ -66,22 +55,23 @@ const ProfileHeader = ({ onFilterValue, filterValue }) => {
   };
 
   const handleShowPage = (user) => {
-    debugger
-    if (user.username) {history.push(`/profile/${user.username}`) ; setShowFollowModal(false); }
-  }
-  debugger
+    if (user.username) {
+      history.push(`/profile/${user.username}`);
+      setShowFollowModal(false);
+    }
+  };
+
   return (
     <>
       <div className="profile-header">
-
         <div className="profile-info">
+          <img className="showPage-profilePageImg" src={showUser.profileImageUrl} />
           <div className="follower-info">
-
-            {currentUser.username !== username &&
+            {currentUser.username !== username && (
               <button onClick={handleFollow}>
                 {isCurrentUserFollower ? 'Unfollow' : 'Follow'}
               </button>
-            }
+            )}
             <button onClick={openModal}>Create A Post</button>
             <div onClick={openFollowModal}>{followers.length} Followers</div>
             <div onClick={openFollowModal}>{following.length} Following</div>
@@ -92,15 +82,15 @@ const ProfileHeader = ({ onFilterValue, filterValue }) => {
                 <span className="close" onClick={closeModal}>
                   &times;
                 </span>
-                < CreatePostModal closeModal={closeModal} />
+                <CreatePostModal closeModal={closeModal} />
               </div>
             </div>
           )}
 
           {showFollowModal && (
-            <div className='follow-modal'>
-              <div className='follow-modal-content'>
-                <div className='modal-header'>
+            <div className="follow-modal">
+              <div className="follow-modal-content">
+                <div className="modal-header">
                   <button
                     className={`toggle-button ${followType === 'followers' ? 'active' : ''}`}
                     onClick={() => setFollowType('followers')}
@@ -114,15 +104,15 @@ const ProfileHeader = ({ onFilterValue, filterValue }) => {
                     Following
                   </button>
                 </div>
-                <div className='modal-body'>
+                <div className="modal-body">
                   {followType === 'followers' && (
-                    <div className='followers-content'>
+                    <div className="followers-content">
                       {followers.map((follower) => {
                         if (follower) {
                           return (
-                            <div className='followers-content-box' key={follower._id} onClick={() => handleShowPage(follower)}>
-                              <img className='followers-content-img' src={follower.profileImageUrl} alt={follower.username} />
-                              <span className='followers-content-box-handle'>{follower.username}</span>
+                            <div className="followers-content-box" key={follower._id} onClick={() => handleShowPage(follower)}>
+                              <img className="followers-content-img" src={follower.profileImageUrl} alt={follower.username} />
+                              <span className="followers-content-box-handle">{follower.username}</span>
                             </div>
                           );
                         }
@@ -131,13 +121,13 @@ const ProfileHeader = ({ onFilterValue, filterValue }) => {
                     </div>
                   )}
                   {followType === 'following' && (
-                    <div className='following-content'>
+                    <div className="following-content">
                       {following.map((celeb) => {
                         if (celeb) {
                           return (
-                            <div className='followers-content-box' key={celeb._id} onClick={() => handleShowPage(celeb)}>
-                              <img className='followers-content-img' src={celeb.profileImageUrl} alt={celeb.username} />
-                              <span className='followers-content-box-handle'>{celeb.username}</span>
+                            <div className="followers-content-box" key={celeb._id} onClick={() => handleShowPage(celeb)}>
+                              <img className="followers-content-img" src={celeb.profileImageUrl} alt={celeb.username} />
+                              <span className="followers-content-box-handle">{celeb.username}</span>
                             </div>
                           );
                         }
@@ -149,30 +139,28 @@ const ProfileHeader = ({ onFilterValue, filterValue }) => {
               </div>
             </div>
           )}
-        </div>
-        <div className="track-filters">
-        <button
-            value="All"
-            onClick={(e) => onFilterValue(e.target.value)}
-            className={filterValue === 'All' ? 'active' : ''}
-          >
-            All
-          </button>
-          <button
-            value="Original"
-            onClick={(e) => onFilterValue(e.target.value)}
-            className={filterValue === 'Original' ? 'active' : ''}
-          >
-            Original
-          </button>
-          <button
-            value="Reposts"
-            onClick={(e) => onFilterValue(e.target.value)}
-            className={filterValue === 'Reposts' ? 'active' : ''}
-          >
-            Reposts
-          </button>
-      
+          <div className="track-filters">
+            <button
+              value="All"
+              onClick={(e) => onFilterValue(e.target.value)}
+              className={filterValue === 'All' ? 'active' : ''}
+            >
+              All
+            </button>
+            <button
+              value="Original"
+              onClick={(e) => onFilterValue(e.target.value)}
+              className={filterValue === 'Original' ? 'active' : ''}
+            >
+              Original
+            </button>
+            <button
+              value="Reposts"
+              onClick={(e) => onFilterValue(e.target.value)}
+              className={filterValue === 'Reposts' ? 'active' : ''}
+            >
+              Reposts
+            </button>
           </div>
         </div>
         {showModal && (
@@ -191,5 +179,3 @@ const ProfileHeader = ({ onFilterValue, filterValue }) => {
 };
 
 export default ProfileHeader;
-
-
