ce8a2a7ccc23ecf5b424ec30863adda24dd0eab1
Matt Pierson <matt.pierson789@gmail.com>
2023-07-20 15:05:37 -0400

    message

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const debug = require('debug');
+const path = require('path');
 
 const cors = require('cors');
 const csurf = require('csurf');
